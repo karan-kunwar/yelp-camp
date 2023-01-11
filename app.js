bde99c60274ff36eb62ec7a3f0162f77cbab6168
Karan Kunwar <karankunwar59@gmail.com>
2023-01-11 19:59:21 +0530

    Added basic styling

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const path = require('path');
 const mongoose = require('mongoose');
 const { handle } = require('express/lib/application');
 const { error } = require('console');
+const ejsMate = require('ejs-mate')
 const Campground = require('./models/campground')
+const methodOverride = require('method-override');
+
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp');
 mongoose.set('strictQuery', false);
@@ -15,26 +18,66 @@ db.once("open", () => {
 });
 
 
-
 const app = express();
+app.engine('ejs', ejsMate);
+app.use(express.urlencoded());
+app.use(methodOverride('_method'));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+
+// Index
 app.get('/', (req, res)=>{
       res.render('home');
 });
 
+// List all campgrounds
 app.get('/campgrounds', async (req, res)=>{
       const campground = await Campground.find({});
       res.render('campgrounds/index', {campground});
 });
 
+
+// Create
+app.get('/campgrounds/new', async (req, res)=>{
+      res.render('campgrounds/new');
+});
+
+app.post('/campgrounds', async(req, res)=>{
+      const campground = await new Campground(req.body.campground);
+      await campground.save();
+      res.redirect(`/campgrounds/${campground._id}`);
+});
+
+
+// Show single Campground
 app.get('/campgrounds/:id', async (req, res)=>{
       const campground = await Campground.findById(req.params.id);
       res.render('campgrounds/show', {campground});
 });
 
+
+// Update
+app.get('/campgrounds/:id/edit', async(req, res)=>{
+      const campground = await Campground.findById(req.params.id);
+      res.render('campgrounds/edit', {campground});
+});
+
+
+app.put('/campgrounds/:id', async(req, res)=>{
+      const { id } = req.params;
+      const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground});
+      res.redirect(`/campgrounds/${campground._id}`);
+});
+
+
+// Delete
+app.delete('/campgrounds/:id', async(req, res)=>{
+      const {id} = req.params;
+      await Campground.findByIdAndRemove(id);
+      res.redirect('/campgrounds');
+});
 
 app.listen(3000, ()=>{
       console.log("Serving on port:3000!");
--- seeds/index.js
+++ seeds/index.js
@@ -21,7 +21,10 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            image:  'https://source.unsplash.com/random/?camping',
+            title: `${sample(descriptors)} ${sample(places)}`,
+            description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi, eos neque, consequuntur vel corporis esse asperiores dignissimos at tempore perferendis laudantium aspernatur culpa incidunt maiores quia corrupti nostrum, aliquam pariatur. Aliquid reprehenderit fuga ipsa ipsam optio, laborum fugiat laudantium sunt doloremque harum sint at deserunt porro vel veniam esse incidunt exercitationem beatae? Doloribus ullam temporibus perferendis aliquam quia eligendi iure.',
+            price: 1000
         })
         await camp.save();
     }
